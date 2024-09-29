64dc0498e02241a88a2a8d87d53a2f1d0fd3ced3
giovanna <giovannalavallesalazr@gmail.com>
2024-09-29 19:35:09 -0400

    Created navbar Icons and logo

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Icon, Button } from "semantic-ui-react";
+import macrameLogoImage from "../Media/NewLogo.jpg";
+import "./Navbar.css";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="Navbar-Container">
+        <div className="Navbar-Menu">
+          <Button>
+            <Icon name="bars" />
+          </Button>
+        </div>
+        <div>
+          <img
+            className="Navbar-Logo"
+            src={macrameLogoImage}
+            alt="MacrameLogo"
+          />
+        </div>
+        <div className="Navbar-Shopping-Cart">
+          <Button>
+            <Icon name="shop" />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
